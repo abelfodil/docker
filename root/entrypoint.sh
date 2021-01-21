dfc3a38db12154a3eb21c73da6722b70a41f9681
Anes Belfodil <ans.belfodil@gmail.com>
2021-01-21 10:45:30 -0500

    Early exit if can't read file

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -5,6 +5,7 @@ for env_var in $FILE_ENV_VARS; do
     var_name="$(echo $env_var | grep -o '.*__FILE=' | sed 's/__FILE=//g')"
     file_path="$(echo $env_var | grep -o '__FILE=.*' | sed 's/__FILE=//g')"
     file_content="$(cat $file_path)"
+    [[ ! $? -eq 0 ]] && exit 1 # Exit if last command failed
     new_var="$(echo $var_name=$file_content)"
     export $(echo $new_var | xargs)
 done
